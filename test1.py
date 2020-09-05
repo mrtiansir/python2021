d6ad5c9d728411ce798c3914e3ab972bdbef0dfc
mrtiansir <657036136@qq.com>
2020-09-05 14:35:59 +0800

    commit 2

--- test1.py
+++ test1.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 # _*_ coding=utf-8 _*_
 
+import random
+
+section1 = random.randint(1,255)
+section2 = random.randint(1,255)
+section3 = random.randint(1,255)
+section4 = random.randint(1,255)
+
+print(str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4))
 
 if __name__ == '__main__':
     pass
