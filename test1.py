5bcf907ffff56ea9074ca0fe65056a8b026b4503
mrtiansir <657036136@qq.com>
2020-09-05 14:37:52 +0800

    commit 3

--- test1.py
+++ test1.py
@@ -3,13 +3,13 @@
 
 import random
 
-section1 = random.randint(1,255)
-section2 = random.randint(1,255)
-section3 = random.randint(1,255)
-section4 = random.randint(1,255)
+section1 = random.randint(1, 255)
+section2 = random.randint(1, 255)
+section3 = random.randint(1, 255)
+section4 = random.randint(1, 255)
 
 print(str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4))
 
 if __name__ == '__main__':
     pass
-    
+
